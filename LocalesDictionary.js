9f6884c905a72505f0bc8388c891f08ae5b06b31
ttokdev <ttokdev@gmail.com>
2021-08-05 23:02:21 +0900

    翻訳ソースの階層化

---
+++ LocalesDictionary.js
@@ -0,0 +1,75 @@
+export const dict = 
+[
+  {
+    "LP": {
+      "Copy": {
+        "en": "apple",
+        "ja": "りんご"
+      },
+      "Greeting": {
+        "en": "orange",
+        "ja": "オレンジ"
+      }
+    },
+  },
+  {
+    "Onboarding1": {
+      "Heading1": {
+        "en": "headin2",
+        "ja": "見出し1"
+      },
+      "Title": {
+      "en": "Onboarding1",
+      "ja": "オンボーディング1"
+      }
+    }
+  },
+  {
+    "Onboarding2": {
+      "Heading2": {
+        "en": "headin2",
+        "ja": "見出し2"
+      },
+      "Title": {
+      "en": "Onboarding2",
+      "ja": "オンボーディング2"
+      }
+    }
+  },
+  {
+    "dashboard": {
+      "text1": {
+        "en": "text1",
+        "ja": "テキスト1"
+      },
+      "text2": {
+      "en": "text2",
+      "ja": "テキスト2"
+      }
+    }
+  },
+  {
+    "header": {
+      "Login": {
+        "en": "Log in",
+        "ja": "ログイン"
+      },
+      "text2": {
+      "en": "text2",
+      "ja": "テキスト2"
+      }
+    }
+  },
+  {
+    "footer": {
+      "TranslateTest": {
+        "en": "This is English Test",
+        "ja": "これは日本語のテストです"
+      },
+      "TranslateTest2": {
+        "en": "This is English Test2",
+        "ja": "これは日本語のテスト2です"
+      },
+    }
+  }
+]
