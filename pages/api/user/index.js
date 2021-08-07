c5068082fa777cc02b335107adeb9f1063455b41
skonno <19647466+shun-konno@users.noreply.github.com>
2021-08-07 13:22:29 +0900

    add user api

---
+++ pages/api/user/index.js
@@ -0,0 +1,14 @@
+import { fetchUser } from '../../../lib/db-admin'
+import { authAdmin } from '../../../lib/firebase-admin'
+
+const user = async (req, res) => {
+  try {
+    const { uid } = await authAdmin.verifyIdToken(req.headers.token)
+    const user = await fetchUser(uid)
+    res.status(200).json(user)
+  } catch (error) {
+    res.status(500).json({ error })
+  }
+}
+
+export default user
