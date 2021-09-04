af57df42e50ea213db8543a715888bdaf849b208
ttokdev <48268149+ttokdev@users.noreply.github.com>
2021-09-04 15:56:07 +0900

    リダイレクト用のemptyページの作成

---
+++ pages/empty.js
@@ -0,0 +1,12 @@
+import Head from 'next/head'
+
+export default function Empty() {
+  return (
+    <>
+      <Head>
+        <title>Empty</title>
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+    </>
+  )
+}
