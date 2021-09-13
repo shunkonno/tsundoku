4273b1724bfbea0701e52517cb475e8841adcd43
skonno <19647466+shun-konno@users.noreply.github.com>
2021-09-13 12:15:40 +0900

    add function and comment out

--- pages/session/[sessionId]/join.js
+++ pages/session/[sessionId]/join.js
@@ -3,7 +3,12 @@
 // ============================================================
 import Head from 'next/head'
 import { useRouter } from 'next/router'
+// import useSWR from 'swr'
 import Script from 'next/script'
+
+// Functions
+// import { useAuth } from '../../../lib/auth'
+// import fetcher from '../../../utils/fetcher'
 
 export default function Session() {
   // ============================================================
@@ -13,22 +18,54 @@ export default function Session() {
   const { sessionId } = router.query
 
   // ============================================================
+  // Auth
+  // ============================================================
+
+  // const auth = useAuth()
+  // const user = auth.user
+
+  // ============================================================
+  // Fetch Data
+  // ============================================================
+
+  // // Fetch logged user info on client side
+  // const { data: userInfo } = useSWR(
+  //   user ? ['/api/user', user.token] : null,
+  //   fetcher,
+  //   {
+  //     onErrorRetry: ({ retryCount }) => {
+  //       // Retry up to 10 times
+  //       if (retryCount >= 10) return
+  //     }
+  //   }
+  // )
+
+  // ============================================================
   // Initialize Video Call
   // ============================================================
   if (typeof window !== 'undefined') {
     callDailyIframe()
+    // if (userInfo) {
+    //   callDailyIframe(userInfo.name)
+    // }
   }
 
   function callDailyIframe() {
     const callFrame = window.DailyIframe.createFrame({
       // TODO: Change language based on locale (https://docs.daily.co/reference#properties)
+      url: 'https://tsundoku.daily.co/' + sessionId,
       lang: 'jp',
       showLeaveButton: true,
+      // showLocalVideo: false,
+      // videoSource: false,
       iframeStyle: { position: 'fixed', width: '100%', height: '100%' }
     })
 
     // TODO: Set domain config with REST API (redirect_on_meeting_exit)
-    callFrame.join({ url: 'https://tsundoku.daily.co/' + sessionId })
+    // callFrame.join({
+    //   userName: userName
+    // })
+    callFrame.join()
   }
 
   // ============================================================
