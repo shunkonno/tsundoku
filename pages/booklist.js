8ca4b0bea29482375763e75f5632b00cf26c550e
ttokdev <48268149+ttokdev@users.noreply.github.com>
2021-09-21 23:56:13 +0900

    booklistページ作成

---
+++ pages/booklist.js
@@ -0,0 +1,81 @@
+// ============================================================
+// Imports
+// ===========================================================
+import Head from 'next/head'
+import useSWR from 'swr'
+
+// Components
+import { AppHeader } from '../components/Header'
+import { Footer } from '../components/Footer'
+import { Navbar } from '../components/Navbar'
+
+// Functions
+import { useAuth } from '../lib/auth'
+import fetcher from '../utils/fetcher'
+import uselocalesFilter from '../utils/translate'
+
+export default function BookList() {
+  // ============================================================
+  // Auth
+  // ============================================================
+
+  const auth = useAuth()
+  const user = auth.user
+
+  // ============================================================
+  // Fetch Data
+  // ============================================================
+
+  // Fetch logged user info on client side
+  const { data: userInfo } = useSWR(
+    user ? ['/api/user', user.token] : null,
+    fetcher,
+    {
+      onErrorRetry: ({ retryCount }) => {
+        // Retry up to 10 times
+        if (retryCount >= 10) return
+      }
+    }
+  )
+
+  // ============================================================
+  // Localization
+  // ============================================================
+  const { locale } = router
+  const t = uselocalesFilter('booklist', locale)
+
+  // ============================================================
+  // Return
+  // ============================================================
+  if (user === null || !userInfo || !sessions) {
+    return <div>Waiting..</div>
+  }
+
+  return (
+    <div>
+      <Head>
+        <title>Tsundoku | ブックリスト</title>
+        <meta
+          name="description"
+          content=""
+        />
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+
+      <AppHeader />
+
+      {/* main content */}
+      <div className="relative pb-16 bg-gray-50 overflow-hidden">
+        <div className="sm:block sm:h-full sm:w-full" aria-hidden="true">
+          <main className="relative mx-auto max-w-7xl px-4 sm:py-4">
+            <Navbar />
+            
+          </main>
+        </div>
+      </div>
+
+      <Footer />
+      
+    </div>
+  )
+}
