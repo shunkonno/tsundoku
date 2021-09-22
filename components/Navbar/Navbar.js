90fe27a57b86271af268aac30540f78afccc7c9b
ttokdev <48268149+ttokdev@users.noreply.github.com>
2021-09-22 11:00:33 +0900

    Navbarのリンク実装

--- components/Navbar/Navbar.js
+++ components/Navbar/Navbar.js
@@ -1,34 +1,55 @@
 
-const tabs = [
-  { name: 'ホーム', href: '#', current: true },
-  { name: 'ブックリスト', href: '#', current: false },
-  { name: '人気の本', href: '#', current: false },
-]
+import { HomeIcon, BookOpenIcon, UserGroupIcon } from '@heroicons/react/outline'
+import Link from 'next/link'
 
 function classNames(...classes) {
   return classes.filter(Boolean).join(' ')
 }
 
+var currentPage
+if (typeof window !== "undefined") {
+  // windowを使う処理を記述
+  currentPage = window.location.pathname.split('/').slice(-1)[0]
+}
+
+
+console.log(currentPage)
+
+const tabs = [
+  { name: 'ホーム', href: '/home', icon: HomeIcon, current: currentPage == 'home' },
+  { name: 'ブックリスト', href: '/booklist', icon: BookOpenIcon, current: currentPage == 'booklist' },
+  { name: '人気の本', href: '/trend', icon: UserGroupIcon, current: currentPage == 'trend' },
+]
+
 export default function Navbar() {
   return (
     <div>
       <div className="hidden sm:block">
-        <div className="border-b border-gray-200">
+        <div className="">
           <nav className="-mb-px flex justify-center" aria-label="Tabs">
             {tabs.map((tab) => (
-              <a
-                key={tab.name}
-                href={tab.href}
-                className={classNames(
-                  tab.current
-                    ? 'border-indigo-500 text-indigo-600'
-                    : 'border-transparent text-gray-500 hover:text-gray-700 hover:border-gray-300',
-                  'w-1/4 py-4 px-1 text-center border-b-2 font-medium text-sm'
-                )}
-                aria-current={tab.current ? 'page' : undefined}
-              >
-                {tab.name}
-              </a>
+              <div key={tab.name} className="group w-1/4">
+                <Link href={tab.href}>
+                <a
+                  className={classNames(
+                    tab.current
+                      ? 'border-tsundoku-blue-main text-tsundoku-blue-main'
+                      : 'border-transparent text-gray-500 group-hover:text-gray-700 group-hover:border-gray-300',
+                    'flex items-center justify-center py-4  border-b-2 border-gray-200 font-medium text-sm'
+                  )}
+                  aria-current={tab.current ? 'page' : undefined}
+                >
+                  <tab.icon
+                    className={classNames(
+                      tab.current ? 'text-tsundoku-blue-main' : 'text-gray-500 group-hover:text-gray-700',
+                      'inline-block -ml-0.5 mr-2 h-5 w-5'
+                    )}
+                    aria-hidden="true"
+                  />
+                  <span>{tab.name}</span>
+                </a>
+                </Link>
+                </div>
             ))}
           </nav>
         </div>
