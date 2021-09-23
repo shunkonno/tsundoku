db9938c720c079b08c5862793cedf59533b4e45a
ttokdev <48268149+ttokdev@users.noreply.github.com>
2021-09-24 05:55:01 +0900

    next.config 外部ドメインの画像設定

--- next.config.js
+++ next.config.js
@@ -1,5 +1,8 @@
 module.exports = {
   reactStrictMode: true,
+  images: {
+    domains: ['books.google.com','images.unsplash.com'],
+  },
   i18n: {
     locales: ['ja', 'en'],
     defaultLocale: 'en',
