e5d9f859dd1a45e6c0417f80755fa946b680fc9e
ttokdev <48268149+ttokdev@users.noreply.github.com>
2021-09-24 05:56:47 +0900

    tailwind.configにheightのutilityを追加

--- tailwind.config.js
+++ tailwind.config.js
@@ -32,11 +32,24 @@ module.exports = {
         }
       }
     },
+    
     extend: {
       fontFamily: {
         NotoSerif: ['Noto Serif JP'],
         NotoSans: ['Noto Sans JP']
-      }
+      },
+      height: {
+				"10v": "10vh",
+				"20v": "20vh",
+				"30v": "30vh",
+				"40v": "40vh",
+				"50v": "50vh",
+				"60v": "60vh",
+				"70v": "70vh",
+				"80v": "80vh",
+				"90v": "90vh",
+				"100v": "100vh",
+			},
     }
   },
   variants: {
