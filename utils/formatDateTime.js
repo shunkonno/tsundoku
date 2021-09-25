d014c3a95e33293e45c584fc40d5557dafcf61e0
ttokdev <48268149+ttokdev@users.noreply.github.com>
2021-09-25 12:53:13 +0900

    moment系関数を外部ファイルに切り分け

---
+++ utils/formatDateTime.js
@@ -0,0 +1,15 @@
+import moment from 'moment'
+
+// ISOString を 月、日、時、分 に変換 (例：10月12日 12:45)
+export const formatISOStringToDateTime = (IsoString) => {
+  return moment(IsoString).format('M月D日 H:mm')
+}
+// ISOString を 月、日 に変換 (例：10月12日)
+export const formatISOStringToDate = (IsoString) => {
+  return moment(IsoString).format('M月D日')
+}
+
+// ISOString を 時、分 に変換 (例：12:45)
+export const formatISOStringToTime = (IsoString) => {
+  return moment(IsoString).format('H:mm')
+}
