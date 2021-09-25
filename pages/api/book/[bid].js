951038a50306a2c7f31d550db4d380fdf2686b5d
skonno <19647466+shun-konno@users.noreply.github.com>
2021-09-25 10:22:24 +0900

    modify book api to fetch with bid

--- pages/api/book/[bid].js
+++ pages/api/book/[bid].js
@@ -2,7 +2,7 @@ import { fetchOneBook } from '../../../lib/db-admin'
 
 const bookApiHandler = async (req, res) => {
   try {
-    const book = await fetchOneBook(req.query.isbn13)
+    const book = await fetchOneBook(req.query.bid)
     res.status(200).json(book)
   } catch (error) {
     res.status(500).json({ error })
