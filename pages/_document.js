860749b0086379dfcb1322d038a3b9e6f7c62043
skonno <19647466+shun-konno@users.noreply.github.com>
2021-10-02 00:37:51 +0900

    add font settings

--- pages/_document.js
+++ pages/_document.js
@@ -20,6 +20,13 @@ export default class MyDocument extends Document {
           `
             }}
           />
+          {/* Font */}
+          <link rel="preconnect" href="https://fonts.googleapis.com" />
+          <link rel="preconnect" href="https://fonts.gstatic.com" />
+          <link
+            href="https://fonts.googleapis.com/css2?family=Noto+Sans+JP&display=swap"
+            rel="stylesheet"
+          />
         </Head>
         <body>
           <Main />
