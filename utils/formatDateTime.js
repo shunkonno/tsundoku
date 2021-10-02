cee9ca6f7deb57cc8d22ae31df4de66b652994f4
skonno <19647466+shun-konno@users.noreply.github.com>
2021-10-03 01:54:20 +0900

    minor change

--- utils/formatDateTime.js
+++ utils/formatDateTime.js
@@ -2,12 +2,12 @@ import moment from 'moment'
 
 // ISOString を 月、日、時、分 に変換 (例：10月12日 12:45)
 export const formatISOStringToDateTime = (IsoString) => {
-  return moment(IsoString).format('M月D日 H:mm')
+  return moment(IsoString).format('M月D日 HH:mm')
 }
 
 // ISOString を 月、日 に変換 (例：10月12日)
 export const formatISOStringToDateTimeWithSlash = (IsoString) => {
-  return moment(IsoString).format('YYYY/M/D H:mm')
+  return moment(IsoString).format('YYYY/M/D HH:mm')
 }
 
 // ISOString を 月、日 に変換 (例：10月12日)
@@ -17,5 +17,5 @@ export const formatISOStringToDate = (IsoString) => {
 
 // ISOString を 時、分 に変換 (例：12:45)
 export const formatISOStringToTime = (IsoString) => {
-  return moment(IsoString).format('H:mm')
+  return moment(IsoString).format('HH:mm')
 }
