12b4a3f6ac1552b4f599ce90832722945e07b0f6
ttokdev <48268149+ttokdev@users.noreply.github.com>
2021-10-07 23:03:07 +0900

    BookProgressIconの読了度を4段階に修正

--- components/Icon/BookProgressIcon.js
+++ components/Icon/BookProgressIcon.js
@@ -2,23 +2,10 @@ import classNames from "../../utils/classNames"
 
 export default function BookProgressIcon({progress}) {
   return(
-    <div className="flex">
+    <div className="flex justify-center">
       <svg
         className={classNames(
           progress >= 0.2 ?
-          "text-blue-100"
-          :
-          "text-gray-100",
-          "mr-1 w-1.5 h-6"
-        )}
-        fill="currentColor"
-        viewBox="0 0 5 20"
-      >
-        <rect x="0" y="0" r="1" width="5" height="20" />
-      </svg>
-      <svg
-        className={classNames(
-          progress >= 0.4 ?
           "text-blue-200"
           :
           "text-gray-100",
@@ -31,7 +18,7 @@ export default function BookProgressIcon({progress}) {
       </svg>
       <svg
         className={classNames(
-          progress >= 0.6 ?
+          progress >= 0.4 ?
           "text-blue-300"
           :
           "text-gray-100",
@@ -44,7 +31,7 @@ export default function BookProgressIcon({progress}) {
       </svg>
       <svg
         className={classNames(
-          progress >= 0.8 ?
+          progress >= 0.7 ?
           "text-blue-400"
           :
           "text-gray-100",
