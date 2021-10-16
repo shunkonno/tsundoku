9c9bba4fb13df1925f59964ce3cb5f41dee4cf68
skonno <19647466+shun-konno@users.noreply.github.com>
2021-10-16 12:44:59 +0900

    minor change

--- components/Footer/LpFooter.js
+++ components/Footer/LpFooter.js
@@ -214,9 +214,9 @@ export default function LpFooter() {
           </div>
         </div> */}
         <div className="md:flex md:justify-end md:items-center pt-4 border-t border-blueGray-700">
-          <p className="mt-8 md:mt-0 text-base text-gray-400">
+          {/* <p className="mt-8 md:mt-0 text-base text-gray-400">
             &copy; 2021 Tsundoku All rights reserved.
-          </p>
+          </p> */}
           <div className="flex items-center">
             <div className="hidden sm:block my-4">
               {user ? (
