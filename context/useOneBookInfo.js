f9687f812e290029f757a0c655d72f18dc63c83e
ttokdev <48268149+ttokdev@users.noreply.github.com>
2021-11-04 06:59:43 +0900

    Create useOneBookInfo.js

---
+++ context/useOneBookInfo.js
@@ -0,0 +1,20 @@
+import useSWR from 'swr'
+
+import fetcher from '../utils/fetcher'
+
+export const useOneBookInfo = (bid) => {
+
+  // ユーザー情報
+  const { data: oneBookInfo } = useSWR(
+    bid ? '/api/book/' + bid : null,
+    fetcher,
+    {
+      onErrorRetry: ({ retryCount }) => {
+        // エラー時には、10回まではリトライする
+        if (retryCount >= 10) return
+      }
+    }
+  )
+
+  return oneBookInfo
+}
