50a820f5583e9c9ec7e57feefcdb0958a51c3e09
ttokdev <48268149+ttokdev@users.noreply.github.com>
2021-11-03 22:16:41 +0900

    Sectionコンポーネント作成

---
+++ components/Section/index.js
@@ -0,0 +1,6 @@
+import ReservedRoomSection from './ReservedRoomSection'
+import ReservableRoomSection from './ReservableRoomSection'
+import HomeIsReadingSection from './HomeIsReadingSection'
+import HomeReadTimeSection  from './HomeReadTimeSection'
+
+export { ReservedRoomSection, ReservableRoomSection, HomeIsReadingSection, HomeReadTimeSection  }
